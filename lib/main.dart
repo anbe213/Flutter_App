d3922a1e165f3c355e2390d23846641172e2ed4e
March <xuanphu213@gmail.com>
2021-01-28 00:02:11 +0700

    Update 'player name' Box and 'move log' Box

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,14 @@ class MyHomePage extends StatefulWidget{
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class Controller extends ChessBoardController {
+
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+  List<String> moveLog = [];
+  String whitePlayerName = 'White Player', blackPlayerName = 'Black Player';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,19 +38,58 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
+            SizedBox(
+              width: double.infinity,
+              child: Container(
+                padding: EdgeInsets.fromLTRB(10, 10, 10 , 10),
+                height: 50,
+                color: Colors.brown,
+                child: Text(
+                  '$blackPlayerName', textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ),
             ChessBoard(
                 onMove: (move) {
                   print(move);
+                  /*setState(() {
+                    moveLog.add(move);
+                  });*/
                 },
                 onCheckMate: (color) {
                   print(color);
                 },
-                onDraw: () {},
+                onDraw: () {
+                  print("DRAW!!!");
+                },
               size: MediaQuery.of(context).size.width,
               enableUserMoves: true,
               boardType: BoardType.orange,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Container(
+                padding: EdgeInsets.fromLTRB(10, 10, 10 , 10),
+                height: 50,
+                color: Colors.brown,
+                child: Text(
+                  '$whitePlayerName', textAlign: TextAlign.right,
+                  style: TextStyle(fontSize: 25),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: Container(
+                height: 186,
+                color: Colors.grey,
+                child: Text(
+                  'Move: $moveLog'
+                ),
+              ),
             )
           ],
         ),
